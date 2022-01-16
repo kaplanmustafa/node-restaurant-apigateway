1c09c464f242a36b58d7008be991096d08f162d1
muskaplan <muskaplan@innova.com.tr>
2022-01-16 14:52:36 +0300

    application.yml configurations

--- src/main/java/com/example/noderestaurantapigateway/NodeRestaurantApigatewayApplication.java
+++ src/main/java/com/example/noderestaurantapigateway/NodeRestaurantApigatewayApplication.java
@@ -2,12 +2,14 @@ package com.example.noderestaurantapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NodeRestaurantApigatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NodeRestaurantApigatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NodeRestaurantApigatewayApplication.class, args);
+    }
 
 }
